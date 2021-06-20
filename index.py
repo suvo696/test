c202760d218dda0728ec643f6851f1dcdf278ac9
suvo696 <18bec062@smvdu.ac.in>
2021-06-20 23:53:52 +0530

    new

--- index.py
+++ index.py
@@ -1,10 +1,27 @@
 from flask import Flask,render_template
-
+from bs4 import BeautifulSoup
+from selenium import webdriver
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    url = 'https://marketsmithindia.com/mstool/eval/RELIANCE/evaluation.jsp#/'
+    driver = webdriver.Chrome("E:/chromedriver_win32/chromedriver.exe")
+    driver.get(url) 
+
+
+    content = driver.page_source
+    soup = BeautifulSoup(content, features = "lxml")
+    v=(soup.findAll('td',class_='surveillancetext'))
+    l=[]
+    for i in v:
+        try:
+            l.append(i.text)
+        except:
+            l.append(i.findAll('a').text)
+    driver.close()
+
+    return render_template('index.html',l=l)
 
 
 if __name__ == "__main__":
