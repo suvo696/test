dde72752da7136a00ecc246384581bdd64168bb3
suvo696 <18bec062@smvdu.ac.in>
2021-06-21 00:47:38 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,23 +1,34 @@
 from flask import Flask,render_template
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
+import os
 app=Flask(__name__)
 
 @app.route("/")
 def home():
+    url = 'https://marketsmithindia.com/mstool/eval/RELIANCE/evaluation.jsp#/'
     
-    gChromeOptions = webdriver.ChromeOptions()
-    gChromeOptions.add_argument("window-size=1920x1480")
-    gChromeOptions.add_argument("disable-dev-shm-usage")
-    gDriver = webdriver.Chrome(
-        chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
-    )
-    gDriver.get("https://www.python.org/")
-    
-    gDriver.save_screenshot("my_screenshot.png")
-    gDriver.close()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+    # Now you can start using Selenium
+    driver.get(url) 
+
+
+    content = driver.page_source
+    soup = BeautifulSoup(content, features = "lxml")
+    v=(soup.findAll('td',class_='surveillancetext'))
+    l=[]
+    for i in v:
+        try:
+            l.append(i.text)
+        except:
+            l.append(i.findAll('a').text)
+    driver.close()
 
     return render_template('index.html',l=l)
 
